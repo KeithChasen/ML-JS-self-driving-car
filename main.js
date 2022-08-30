280b9cfc89491b65bf5cf612defc4b08a6bc4f72
keithchasen <keithchasen89@gmail.com>
2022-08-30 17:47:58 +0300

    put car in the middle of a lane

--- main.js
+++ main.js
@@ -2,9 +2,11 @@ const canvas = document.getElementById('myCanvas');
 canvas.width = 200;
 
 const ctx = canvas.getContext('2d');
-const car = new Car(100, 100, 30, 50);
-
 const road = new Road(canvas.width / 2, canvas.width * .9);
+
+// put car in a 0 (left) lane
+const xCarPosition = road.getLaneCenter(4);
+const car = new Car(xCarPosition, 100, 30, 50);
 
 animate();
 
