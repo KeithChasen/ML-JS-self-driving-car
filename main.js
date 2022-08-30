90303ead792486a4b19d0a7e01943eb9c3e38377
keithchasen <keithchasen89@gmail.com>
2022-08-30 16:37:06 +0300

    basic road

--- main.js
+++ main.js
@@ -4,11 +4,14 @@ canvas.width = 200;
 const ctx = canvas.getContext('2d');
 const car = new Car(100, 100, 30, 50);
 
+const road = new Road(canvas.width / 2, canvas.width * .9);
+
 animate();
 
 function animate() {
     car.update();
     canvas.height = window.innerHeight;
+    road.draw(ctx);
     car.draw(ctx);
     requestAnimationFrame(animate);
 }
